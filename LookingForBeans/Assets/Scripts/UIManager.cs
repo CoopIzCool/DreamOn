7b7e0891cbfee63d7940355271ffae52234657fa
MBHayes00 <mbh9947@rit.edu>
2020-12-12 13:43:12 -0500

    Working on main menu

---
+++ LookingForBeans/Assets/Scripts/UIManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Hosting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void ToOptions()
+    {
+
+    }
+
+    public void ToMain()
+    {
+
+    }
+
+}
