baa9edd00f4100e3bcdda84d4a0f5ca1674712d5
EricDelmonico <54674769+EricDelmonico@users.noreply.github.com>
2020-12-11 22:35:36 -0500

    Added a gun
    Added a gun that shoots. The things it shoots need to be rigid bodies

---
+++ LookingForBeans/Assets/Scripts/Bullet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    #region Fields
+
+    private float speed;
+    private float deathTime;
+
+    #endregion
+
+    public void SetParams(float speed, float deathTime)
+    {
+        this.speed = speed;
+        this.deathTime = deathTime;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DeathTimer());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+    IEnumerator DeathTimer()
+    {
+        yield return new WaitForSeconds(deathTime);
+        Destroy(gameObject);
+    }
+}
