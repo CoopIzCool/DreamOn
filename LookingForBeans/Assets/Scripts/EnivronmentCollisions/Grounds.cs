fe537c28a7a3439cedb30e904280fadb43b3986e
Ryan Cooper <ryancooper315@gmail.com>
2020-12-13 11:52:56 -0500

    GameOver and Stupid Fixes

---
+++ LookingForBeans/Assets/Scripts/EnivronmentCollisions/Grounds.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grounds : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (collision.gameObject.GetComponent<PlayerMovement>().FreeFall == true)
+            {
+                collision.gameObject.GetComponent<PlayerMovement>().FreeFall = false;
+                collision.gameObject.GetComponent<PlayerMovement>().Continue = true;
+            }
+        }
+    }
+}
